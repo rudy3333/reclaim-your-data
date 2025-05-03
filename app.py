bd3ee222654dfc86bc137eed5d3fc47c1127112e
rudy3333 <mmax3163@gmail.com>
2025-05-03 19:47:31 +0200

    init draft

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 
 app= Flask(__name__)
+
+#serve static css
+@app.route('/styles.css')
+def serve_css():
+    return app.send_static_file('styles.css')
+
 @app.route('/')
 def serve_static():
     return app.send_static_file('index.html')
+
